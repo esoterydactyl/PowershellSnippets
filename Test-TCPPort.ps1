708aba872b41ce5b7e44469d5c32256fc7e6fae2
Esoterydactyl <davidkcaudill@gmail.com>
2014-10-31 14:27:12 -0700

    Port check
    This is a nice object-y replacement for portqry.  Multithread it to make
    a nice port scanner!

---
+++ Test-TCPPort.ps1
@@ -0,0 +1,33 @@
+﻿function Test-TcpPort 
+    {
+        param($hostname,$port)
+
+        try {
+            $tcp = New-Object System.Net.Sockets.TcpClient
+            $tcp.Connect($hostname,$port)
+            $tcp.Close()
+         
+            return New-Object PSObject -Property @{
+                Hostname = $hostname
+                Port = $port
+                Pass = $true
+                Exception = $null
+            }
+        }
+        catch [System.Net.Sockets.SocketException] {
+            return New-Object PSObject -Property @{
+                Hostname = $hostname
+                Port = $port
+                Pass = $false
+                Exception=[string]::format("Socket Exception: {0}", $_)
+            }        
+        }    
+        catch {
+            return New-Object PSObject -Property @{
+                Hostname = $hostname
+                Port = $port
+                Pass = $false
+                Exception = $_
+            }    
+        }
+    }

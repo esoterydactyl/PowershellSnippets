499d310c33d99c9e2c37c610315bc503999a6e5f
Esoterydactyl <davidkcaudill@gmail.com>
2014-10-31 14:42:33 -0700

    functions
    Get-sitestatus does a quick web site check.
    pop-notifcation pops a quick a notification bubble in the tray.

---
+++ Get-SiteStatus.ps1
@@ -0,0 +1,17 @@
+﻿function Get-SiteStatus($url)
+	{
+	    $req = [System.Net.HttpWebRequest]::Create($url)
+	    try
+	    {
+	        $res = $req.GetResponse()
+	        [int]$res.StatusCode
+	    }
+	    catch
+	    {
+	        "Failed"
+	    }
+		finally
+		{
+			$res.Close()
+		}
+	}
